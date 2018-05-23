a2375e8187b65486fd5de3ce9eb5fa13fc1c5815
Karen Gregg <karengregg@karens-mbp>
2018-05-22 19:52:27 -0500

    added textView to CustomeCollectionViewCell

--- MovieMemory/CustomCollectionViewCell.swift
+++ MovieMemory/CustomCollectionViewCell.swift
@@ -12,4 +12,5 @@ class CustomCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
 }
