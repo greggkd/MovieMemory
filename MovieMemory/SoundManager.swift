2e9299489f35e4829e1e77e0e813d421dee37276
Karen Gregg <karengregg@karens-mbp>
2018-05-27 14:56:04 -0500

    started adding audio files

---
+++ MovieMemory/SoundManager.swift
@@ -0,0 +1,16 @@
+//
+//  SoundManager.swift
+//  MovieMemory
+//
+//  Created by Karen Gregg on 5/27/18.
+//  Copyright © 2018 Karen Gregg. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManager{
+    
+    var audioPlayer:AVAudioPlayer?
+}
+
