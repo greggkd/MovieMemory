8af65ba1535d69dc9660db4763c5c90f64733a79
Karen Gregg <karengregg@karens-mbp>
2018-06-25 21:45:10 -0500

    Everything is working, trying to get button pretty

--- MovieMemory/ThanksViewController.swift
+++ MovieMemory/ThanksViewController.swift
@@ -17,6 +17,7 @@ class ThanksViewController: UIViewController {
     //MARK:- Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
     
@@ -39,15 +40,5 @@ class ThanksViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
